76ad52953555fc9f6bbfca1e47bed790f3fb63f1
Mansher Singh <Manshers-MacBook-Pro.local>
2024-05-01 19:30:33 +0530

    ran the code to get the annotated lyrics

--- FinalProject/annotator.py
+++ FinalProject/annotator.py
@@ -1,5 +1,6 @@
 from transformers import RobertaTokenizerFast, TFRobertaForSequenceClassification, pipeline
 import pandas as pd
+from tqdm import tqdm
 
 # Load the model
 tokenizer = RobertaTokenizerFast.from_pretrained("arpanghoshal/EmoRoBERTa")
@@ -7,20 +8,26 @@ model = TFRobertaForSequenceClassification.from_pretrained("arpanghoshal/EmoRoBE
 
 # Load the dataset
 # df = pd.read_csv("Eminem_Lyrics.csv", sep='\t', comment='#', encoding="ISO-8859-1")
-df = pd.read_csv(".\EminemLyricsCleaned.csv", sep='\t', comment='#', encoding="ISO-8859-1")
+df = pd.read_csv("SpotifyLyricsCleaned.csv", sep='\t', comment='#', encoding="ISO-8859-1")
 
 # Now we need to go through each song's lyrics one by one and use the model to annotate them with the corresponding emotion
 emotion = pipeline('sentiment-analysis', 
                     model='arpanghoshal/EmoRoBERTa')
 
+# truncate the lyrics to 512 tokens so that when we call the model it doesn't run out of memory
+def truncate(text):
+    return text[:512]
+
+# Annotate the lyrics with the corresponding emotion
+df['text'] = df['text'].apply(truncate)
+
 # We will save the annotated lyrics in a new column in the dataframe
-df['Emotion'] = df['Lyrics'].apply(emotion)
+tqdm.pandas(desc="Annotating Lyrics")
+df['Emotion'] = df['text'].progress_apply(emotion)
 df['Emotion'] = df['Emotion'].apply(lambda x: x[0]['label'])
 
 # Drop unnecessary columns - link
-df = df.drop(columns=['Album_URL'])
-df = df.drop(columns=['Views'])
-df = df.drop(columns=['Release_date'])
+df = df.drop(columns=['link'])
 
 # Save the annotated dataframe to a new CSV file
-df.to_csv('EminemLyricsAnnotated.csv', sep='\t', index=False)
+df.to_csv('SpotifyLyricsAnnotated.csv', sep='\t', index=False)
